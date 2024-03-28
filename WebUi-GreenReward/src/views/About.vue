74784067d0cc693285faff91006f351d4b182dda
jpsbat <jpsbat56@gmail.com>
2024-03-27 22:06:10 -0300

    adição bootstrap

--- WebUi-GreenReward/src/views/About.vue
+++ WebUi-GreenReward/src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="about">
+  <div>
     <h1>This is an about page</h1>
+    <b-button>Teste</b-button>
   </div>
 </template>
+
+<script>
+import BButton from 'bootstrap-vue'
+
+export default {
+  components: {
+    BButton
+  }
+}
+</script>
